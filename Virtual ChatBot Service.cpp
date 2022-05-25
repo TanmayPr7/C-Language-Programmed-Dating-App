6ec575432b22ea4d776f0e035801df09fbc7df73
Tanmay <96858809+TanmayPr7@users.noreply.github.com>
2022-05-25 23:05:55 +0530

    FeedBack Function

--- Virtual ChatBot Service.cpp
+++ Virtual ChatBot Service.cpp
@@ -4,3 +4,28 @@
 #include <cstdlib>
 #include <cstring>
 #include <fstream>
+
+#include <iostream>
+#include <cstring>
+#include <fstream>
+
+void feedback(){            //Function to take feedback from users
+    string fb;                                    
+    ofstream fe("Feedback.txt", fstream::app);
+    cout<<"Enter your Rating for our service please:)"<<endl;
+    cout<<"How many stars do you want to give us on the scale of 1 to 5:)"<<endl;
+    cout<<"                         * * * * *"<<endl;
+    cin>>fb;
+    
+    if(fb.size()<6){
+        cout<<"Your Rating is "<<fb<<", Thank You for giving us your Rating and your Time Sir:)"<<endl;
+        fe<<fb<<endl;
+    }
+    else{
+        cout<<"Your Rating is invalid, please give us valid Rating Sir:)"<<endl;
+        cout<<endl;
+        feedback();
+    }
+    fe.close();
+}
+
