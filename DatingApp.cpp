05340b7f76ab9ff46d8c149eed3c78ef0b964e4e
Tanmay <96858809+TanmayPr7@users.noreply.github.com>
2022-05-25 23:30:43 +0530

    Basic Initialization

---
+++ DatingApp.cpp
@@ -0,0 +1,18 @@
+# This is the basic code for Dating App
+
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+using namespace std;   //Intialization Section
+
+int otp;
+string OTP;
+int dec1;
+int dec2;
+int check;
+void Match();
+
+
+///////////////////////////////////////////////////////Functions & Classes//////////////////////////////////////////////
